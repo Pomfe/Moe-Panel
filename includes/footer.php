d29f5e0a1a288b7543973a292c2ed413418b3d40
Eliot Whalan <ewhal@pantsu.cat>
2016-06-21 17:11:25 +1000

    Modernize footer

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-Amount: <input type="text" name="count" value="30">
-Keyword: <input type="text" name="keyword">
-<input type="submit" value="fetch">
-</form><br>
-<table id="result" style="width:100%">
-<tr><th>ID</th><th>Orginal Name</th><th>Filename</th><th>Size (bytes)</th><th>Action</th></tr>
-</table></body></html>
