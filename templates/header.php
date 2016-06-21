bfee82892be258e1a088be5c5c8f36bd1bc82452
Eliot Whalan <ewhal@pantsu.cat>
2016-06-21 19:31:51 +1000

    Fix up incorrect url

--- templates/header.php
+++ templates/header.php
@@ -33,7 +33,7 @@
 		<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav">
-						<li><a href="<?php echo MOE_URL; ?>/user/panel" target="_BLANK">Moe Panel</a></li>
+						<li><a href="<?php echo MOE_URL; ?>/panel" target="_BLANK">Moe Panel</a></li>
 						<li><a href="<?php echo MOE_URL; ?>/includes/api.php?do=logout">Logout</a></li>
 					</ul>
 				</div>
