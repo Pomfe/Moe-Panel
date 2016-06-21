a7f33d685476ab197eb90039f6c9963442e0cbf6
Eliot Whalan <ewhal@pantsu.cat>
2016-06-21 17:11:06 +1000

    Modernize moderation panel

--- includes/header.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html><html><head><title>Mod</title>
-                    <style>
-                    table,th,td{border:1px solid black; border-collapse:collapse;}
-                th,td{padding:5px;}
-                </style></head><body>
-
