7b7ad96340a5bf17812ccdc98c51757afec5ebba
Eliot Whalan <ewhal@pantsu.cat>
2016-06-22 11:46:59 +1000

    Add reports page

---
+++ templates/reports.php
@@ -0,0 +1,12 @@
+<?php include('header.php');?>
+		<p> Status 0 = not removed</p>
+		<p> Status 1 = removed (not shown)</p>
+		<table id="result" class="table"> 
+		<tr>
+			<th>ID</th>
+			<th>File</th>
+			<th>File ID</th>
+			<th>Reporter</th>
+			<th>Status</th>
+			<th>Action</th>
+		</tr>
